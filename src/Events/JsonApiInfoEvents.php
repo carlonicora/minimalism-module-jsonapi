f866781955184c774019895af3e94fcfb81261f8
Carlo Nicora <carlo.nicora@gmail.com>
2020-05-20 18:08:01 +0100

    LogMessages updates to Events

--- src/Events/JsonApiInfoEvents.php
+++ src/Events/JsonApiInfoEvents.php
@@ -1,36 +1,37 @@
 <?php
 namespace CarloNicora\Minimalism\Modules\JsonApi\Events;
 
-use CarloNicora\Minimalism\Services\Logger\Interfaces\LogMessageInterface;
-use CarloNicora\Minimalism\Services\Logger\LogMessages\InfoLogMessage;
 
-class JsonApiInfoEvents extends InfoLogMessage
+use CarloNicora\Minimalism\Core\Events\Abstracts\AbstractInfoEvent;
+use CarloNicora\Minimalism\Core\Events\Interfaces\EventInterface;
+
+class JsonApiInfoEvents extends AbstractInfoEvent
 {
     /** @var string  */
     protected string $serviceName = 'module-jsonapi';
 
-    public static function TWIG_ENGINE_INITIALISED() : LogMessageInterface
+    public static function TWIG_ENGINE_INITIALISED() : EventInterface
     {
-        return new self(1, 'Twig engine initialised');
+        return new self(1, null, 'Twig engine initialised');
     }
 
-    public static function PRE_RENDER_COMPLETED() : LogMessageInterface
+    public static function PRE_RENDER_COMPLETED() : EventInterface
     {
-        return new self(2, 'Pre render completed successfully');
+        return new self(2, null, 'Pre render completed successfully');
     }
 
-    public static function DATA_GENERATED() : LogMessageInterface
+    public static function DATA_GENERATED() : EventInterface
     {
-        return new self(3, 'Data generated successfully');
+        return new self(3, null, 'Data generated successfully');
     }
 
-    public static function DATA_MERGED(string $viewName) : LogMessageInterface
+    public static function DATA_MERGED(string $viewName) : EventInterface
     {
-        return new self(4, 'Data merged with view %s', [$viewName]);
+        return new self(4, null, 'Data merged with view %s', [$viewName]);
     }
 
-    public static function RENDER_COMPLETE() : LogMessageInterface
+    public static function RENDER_COMPLETE() : EventInterface
     {
-        return new self(5, 'Render complete');
+        return new self(5, null, 'Render complete');
     }
 }
