40ed7bf048e5e7e60a640e33be07fe22a83120ec
Carlo Nicora <carlo.nicora@gmail.com>
2020-05-20 12:41:16 +0100

    generateResponseFromError added

--- src/Traits/JsonApiModelTrait.php
+++ src/Traits/JsonApiModelTrait.php
@@ -2,8 +2,10 @@
 namespace CarloNicora\Minimalism\Modules\JsonApi\Traits;
 
 use CarloNicora\JsonApi\Document;
+use CarloNicora\JsonApi\Objects\Error;
 use CarloNicora\Minimalism\Core\Modules\Interfaces\ResponseInterface;
 use CarloNicora\Minimalism\Modules\JsonApi\JsonApiResponse;
+use Exception;
 
 trait JsonApiModelTrait
 {
@@ -12,7 +14,7 @@ trait JsonApiModelTrait
      * @param string $status
      * @return JsonApiResponse
      */
-    final public function generateResponse(Document $document, string $status) : ResponseInterface
+    public function generateResponse(Document $document, string $status) : ResponseInterface
     {
         $response = new JsonApiResponse();
 
@@ -20,6 +22,23 @@ trait JsonApiModelTrait
         $response->setStatus($status);
 
         $response->setContentType('application/vnd.api+json');
+
+        return $response;
+    }
+
+    /**
+     * @param Exception $e
+     * @return ResponseInterface
+     */
+    public function generateResponseFromError(Exception $e): ResponseInterface
+    {
+        $response = new JsonApiResponse();
+
+        $document = new Document();
+        $document->addError(new Error($e));
+
+        $response->setDocument($document);
+        $response->setStatus($document->errors[0]->status ?? '500');
 
         return $response;
     }
